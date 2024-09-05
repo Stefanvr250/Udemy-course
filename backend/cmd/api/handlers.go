fc835aa30a7133e46571bcad420a69ada06e8ec0
Stefan Janse van Rensburg <stefanvr250@gmail.com>
2024-09-05 13:39:26 +0200

    validate user against database

--- backend/cmd/api/handlers.go
+++ backend/cmd/api/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 )
@@ -43,7 +44,11 @@ func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// validate user against database
-
+	user, err := app.DB.GetUserByEmail(requestPayload.Email)
+	if err != nil {
+		app.errorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
+		return
+	}
 	// check password
 
 	//create jwt user
